b4d14006e7501d9c34872017f1779af92c6b6e22
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-04-13 15:00:33 +1000

    Linted files

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'nextra-theme-docs/style.css'
 import '../public/katex.css'
 
-export default function Nextra({ Component, pageProps }) {
+export default function Nextra ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- theme.config.js
+++ theme.config.js
@@ -8,71 +8,71 @@ export default {
   logo: (
     <>
       <Logo />
-      <div className="px-4 text-gray-600 font-normal hidden md:inline">
+      <div className='px-4 text-gray-600 font-normal hidden md:inline'>
         COMP1511 Revision Session '22
       </div>
     </>
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="COMP1511 Revision Session '22" />
+      <meta name='msapplication-TileColor' content='#ffffff' />
+      <meta name='theme-color' content='#ffffff' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta httpEquiv='Content-Language' content='en' />
+      <meta name='description' content="COMP1511 Revision Session '22" />
       <meta
-        name="og:description"
-        content="Questions with solutions + explanations, sample code and more."
+        name='og:description'
+        content='Questions with solutions + explanations, sample code and more.'
       />
-      <meta name="twitter:card" content="summary_large_image" />
+      <meta name='twitter:card' content='summary_large_image' />
       <meta
-        name="twitter:image"
-        content="https://csesoc-comp1511-revision-2022.vercel.app/og.png"
-      />
-      <meta
-        name="twitter:site:domain"
-        content="https://csesoc-comp1511-revision-2022.vercel.app"
+        name='twitter:image'
+        content='https://csesoc-comp1511-revision-2022.vercel.app/og.png'
       />
       <meta
-        name="twitter:url"
-        content="https://csesoc-comp1511-revision-2022.vercel.app"
+        name='twitter:site:domain'
+        content='https://csesoc-comp1511-revision-2022.vercel.app'
       />
-      <meta name="og:title" content="COMP1511 Revision Session '22" />
       <meta
-        name="og:image"
-        content="https://csesoc-comp1511-revision-2022.vercel.app/og.png"
+        name='twitter:url'
+        content='https://csesoc-comp1511-revision-2022.vercel.app'
       />
-      <meta name="apple-mobile-web-app-title" content="COMP1511" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
+      <meta name='og:title' content="COMP1511 Revision Session '22" />
+      <meta
+        name='og:image'
+        content='https://csesoc-comp1511-revision-2022.vercel.app/og.png'
       />
+      <meta name='apple-mobile-web-app-title' content='COMP1511' />
       <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
+        rel='apple-touch-icon'
+        sizes='180x180'
+        href='/apple-icon-180x180.png'
       />
       <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
+        rel='icon'
+        type='image/png'
+        sizes='192x192'
+        href='/android-icon-192x192.png'
       />
       <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
+        rel='icon'
+        type='image/png'
+        sizes='32x32'
+        href='/favicon-32x32.png'
       />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='96x96'
+        href='/favicon-96x96.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='16x16'
+        href='/favicon-16x16.png'
+      />
+      <meta name='msapplication-TileImage' content='/ms-icon-144x144.png' />
     </>
   ),
   search: true,
@@ -81,5 +81,5 @@ export default {
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
   footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
-  unstable_faviconGlyph: '💻',
+  unstable_faviconGlyph: '💻'
 }
