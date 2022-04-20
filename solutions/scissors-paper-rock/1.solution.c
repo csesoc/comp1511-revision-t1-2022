2f19863689b5f938a91b4df9a2ed4db88ca3aecd
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-04-20 13:17:54 +1000

    Bug fixes for Scissors Paper Rock

--- solutions/scissors-paper-rock/1.solution.c
+++ solutions/scissors-paper-rock/1.solution.c
@@ -3,21 +3,21 @@
 #include <string.h>
 
 // Returns 1 if valid move, 0 if not
-int validmove(char a) {
-    return (a=='S' || a=='R' || a=='P');
-}
+int validmove(char a) { return (a == 'S' || a == 'R' || a == 'P'); }
 
 // Returns 0 if draw, 1 if p1 wins and -1 if p1 loses
-int wld(char p1, char p2) {
-    if (p1 == p2) return 0;
-    if ((p1 == 'S' && p2 == 'P') ||
-        (p1 == 'P' && p2 == 'R') ||
-        (p1 == 'R' && p2 == 'S'))
+int wld(char p1, char p2)
+{
+    if (p1 == p2)
+        return 0;
+    if ((p1 == 'S' && p2 == 'P') || (p1 == 'P' && p2 == 'R')
+        || (p1 == 'R' && p2 == 'S'))
         return 1;
     return -1;
 }
 
-int main (void) {
+int main(void)
+{
     char p1[10];
     char p2[10];
 
@@ -28,12 +28,13 @@ int main (void) {
     fgets(p1, 10, stdin);
     fgets(p2, 10, stdin);
 
-
     // Check each move
     // Note incrementing by 2 so we are ignoring where there should be spaces
-    // In this case, the string RLPUS will still be considered valid
-    for (int i = 0; i < 5; i += 2) {
-        if (!validmove(p1[i]) || !validmove(p2[i])) {
+    // In thahhis case, the string RLPUS will still be considered valid
+    for (int i = 0; i < 5; i += 2)
+    {
+        if (!validmove(p1[i]) || !validmove(p2[i]))
+        {
             printf("Moves invalid!\n");
             return 1;
         }
@@ -41,7 +42,10 @@ int main (void) {
     }
 
     // If p1Score is 0, draw, greater than 0 we have won else lost
-    if (p1Score == 0) printf("It's a draw!\n");
-    if (p1Score < 0) printf("Player 2 won!\n");
-    if (p1Score > 0) printf("Player 1 won!\n");
+    if (p1Score == 0)
+        printf("It's a draw!\n");
+    if (p1Score < 0)
+        printf("Player 2 won!\n");
+    if (p1Score > 0)
+        printf("Player 1 won!\n");
 }
