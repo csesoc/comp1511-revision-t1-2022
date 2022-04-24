bcb7f2e2553a38b8646a2e3c9efb75ddec8a1ddd
Hanyuan Li <hanyuan.work@gmail.com>
2022-04-24 13:35:28 +1000

    fixed up merge conflicts in blackjack

--- solutions/blackjack/solution.py
+++ solutions/blackjack/solution.py
@@ -1,16 +1,10 @@
 n = int(input())
 card = []
-<<<<<<< HEAD
 
 for i in range(n):
     card1,card2 = input(), input()
     card.append([card1, card2])
 
-=======
-for i in range(n):
-    card1,card2 = input(), input()
-    card.append([card1, card2])
->>>>>>> bdb88bdcb354f83fd5cf5dc800c7d5926797b3c6
 def score_cal(cards):
     points = 0
     index = 0
@@ -33,26 +27,17 @@ def score_cal(cards):
                     pts[v] = 0
                 elif int(point)<=16:
                     pts[v] = 1
-<<<<<<< HEAD
-
-=======
->>>>>>> bdb88bdcb354f83fd5cf5dc800c7d5926797b3c6
             i = max(pts, key=pts.get)
         else:
             i = int(i)
 
         points += i
         index += 1
-<<<<<<< HEAD
 
     return points
 
 point = []
 
-=======
-    return points
-point = []
->>>>>>> bdb88bdcb354f83fd5cf5dc800c7d5926797b3c6
 for i in card:
     index = card.index(i)
     points = score_cal(i)
@@ -65,53 +50,35 @@ if any(i==21 for i in point):
             print('Player', no, 'has Black Jack!')
         else:
             continue
-<<<<<<< HEAD
 
         break
-=======
-        break;
->>>>>>> bdb88bdcb354f83fd5cf5dc800c7d5926797b3c6
 else:
     for i in card:
         index = card.index(i)
         pts = point[index]
         index_pt = point.index(pts)
         a = 2
-<<<<<<< HEAD
-
-=======
->>>>>>> bdb88bdcb354f83fd5cf5dc800c7d5926797b3c6
         while pts < 18 and a < 5:
             new_card = input()
             i.append(new_card)
             new_point = score_cal(i)
             pts = new_point
             a += 1
-<<<<<<< HEAD
 
         point[index_pt] = pts
 
     converted_point = []
 
-=======
-        point[index_pt] = pts
-    converted_point = []
->>>>>>> bdb88bdcb354f83fd5cf5dc800c7d5926797b3c6
     for i in point:
         if i > 21 or i <16:
             converted_point.append(0)
         else:
             converted_point.append(i)
-<<<<<<< HEAD
-
-=======
->>>>>>> bdb88bdcb354f83fd5cf5dc800c7d5926797b3c6
     if any(len(i) == 5 for i in card):
         for i in card:
             if len(i) == 5:
                 no = card.index(i) +1    
                 print('Player',no,'has 5-card Charlie')
-<<<<<<< HEAD
                 break
             else:
                 continue
@@ -123,14 +90,3 @@ else:
             no = i + 1
             card_drew = ' '.join(card[i])
             print('Player',no,card_drew)
-=======
-                break;
-            else:
-                   continue
-    else:
-        rank = sorted(range(len(converted_point)), key=lambda k: converted_point[k], reverse = True)
-        for i in rank:
-            no = i + 1
-            card_drew = ' '.join(card[i])
-            print('Player',no,card_drew)
->>>>>>> bdb88bdcb354f83fd5cf5dc800c7d5926797b3c6
